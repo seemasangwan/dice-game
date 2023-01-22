68c0225d97944c4078b49ab869d68b48ad5e0186
Seema <104920728+seemasangwan@users.noreply.github.com>
2023-01-22 13:46:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var r1=Math.floor(Math.random()*6+1);
+console.log(r1);
+var img_1="dice"+r1+".png";
+var left="images/"+img_1;
+var image1=document.querySelectorAll("img")[0];
+//var right_image="images/dice"+r1+".png";
+image1.setAttribute("src",left);
+//document.querySelectorAll("img")[1].setAttribute("src",right_image);
+var r2=Math.floor(Math.random()*6+1);
+var img_2="dice"+r2+".png";
+var right="images/"+img_2;
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",right);
+if(r1>r2)
+{
+    document.querySelector('h1').innerHTML="Plyer1 wins !!! 🎉🎊🎊";
+}
+else if(r2>r1)
+{
+    document.querySelector('h1').innerHTML="Plyer2 wins !!! 🎉🎊🎊";
+
+}
+else 
+{
+    document.querySelector('h1').innerHTML="Oops 😒 tie";
+
+}
